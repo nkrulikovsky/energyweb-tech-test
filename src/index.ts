745b72b001af157c8514719794bee027330ff962
nkrulikovsky <nkrulikovskiy@gmail.com>
2023-04-25 20:59:09 +0300

    solution

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import { Genre, Movie } from "./index.types"
+import { Genre, Movie } from "./index.types";
+import { movies } from "./db.json";
+
+const getRandomNumber = (limit: number): number => {
+  return Math.floor(Math.random() * limit);
+};
 
 export const getFilteredMovies = ({ genres }: { genres: Genre[] }): Movie[] => {
-  return []
-}
+  if (genres.length == 0) return [movies[getRandomNumber(movies.length)]];
+
+    return movies.filter((movie)=> movie.genres.every((genre)=>genres.includes(genre as Genre) )).sort((a,b)=>b.genres.length-a.genres.length)
+};
